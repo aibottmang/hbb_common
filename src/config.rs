3cabd12b9973998743d74914fba5bbce50f5c7fe
aibottmang <ftabacco@pm.me>
2026-01-29 21:06:57 +0100

    Configure custom RustDesk server 93.51.43.12

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["93.51.43.12"];
+pub const RS_PUB_KEY: &str = "DUdup1I22OgH7HkCqXjbIgvtW+wVztxaDnQVmPbcWCo=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
